75fe3719c25fb2a41cfaf0be2a42b7c9b5d6b9c1
Braden Marshall <bmars385@d127.org>
2022-10-19 10:23:30 -0500

    added textfield, label, button added constraints. When a number was entered into the text field, the button was pressed, the number from the text field would be displayed on the label

--- Display A #/ViewController.swift
+++ Display A #/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var numberTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func displayWhenPressed(_ sender: UIButton) {
+        numberLabel.text = numberTextField.text
+    }
+    
 }
 
